da777b021e69db9bdbf09746e70f3fe00e1f53ee
WeslleyFratini <weeslleey354@gmail.com>
2021-06-30 21:53:04 -0300

    authContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import { recoverUserInformation, signInRequest } from "../service/auth";
+import Router from "next/router";
+import { setCookie, parseCookies } from "nookies";
+
+type User = {
+  name;
+  email;
+  avatar_url;
+};
+
+type SignInData = {
+  email: string;
+  password: string;
+};
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  user: User;
+  signIn: (data: SignInData) => Promise<void>;
+};
+
+const AuthContext = createContext({} as AuthContextType);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      recoverUserInformation().then((response) => {
+        setUser(response.user);
+      });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInData) {
+    const { token, user } = await signInRequest({
+      email,
+      password,
+    });
+
+    setCookie(undefined, "nextauth.token", token, {
+      maxAge: 60 * 60 * 1, // 1 hour
+    });
+
+    setUser(user);
+
+    Router.push("/dashboard");
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
